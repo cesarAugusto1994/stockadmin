d1aa5f1b0bcf5baee301f986de37d0c1c3b8856a
Cesar Augusto <cezzaar@gmail.com>
2018-03-23 23:00:18 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1>Painel Principal</h1>
+@stop
+
+@section('content')
+    <p>Seus painéis aparecerão aqui!</p>
+@stop
